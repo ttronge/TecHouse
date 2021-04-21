b9a4e2844717d5e9a33bc8f5caccd36e0627489a
ttronge <tomastronge@gmail.com>
2021-04-21 09:57:35 -0300

    update

--- front/src/components/card/cardStyle.js
+++ front/src/components/card/cardStyle.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles({
     root: {
-        maxWidth: 340,
+        maxWidth: 500,
     },
     media: {
         height: 200,
